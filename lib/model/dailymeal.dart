8a9d4cf837a48af5f058894e290c591d49da85f7
T0269931 <ahmed.dahir2019@my.ntu.ac.uk>
2023-03-24 19:52:45 +0000

    added changes

---
+++ lib/model/dailymeal.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+import 'package:myfitnessapp/model/products.dart';
+
+import 'dailytrack.dart';
+
+
+
+class dailymeal {
+  dailytrack mealfor_day;
+
+  Product product;
+
+
+
+  dailymeal({
+   required this.product,
+    required this.mealfor_day
+
+  });
+
+
+
+// dailytrack.fromSnapshot(snapshot)
+//     : full_name = snapshot.data()["full_name"],
+//       company = snapshot.data()["company"],
+//       age = snapshot.data()["age"];
+//       dateTime =
+
+
+}
+
